40e78e224754bdeb38bb2e64405d3fbd03342b38
Sandra Noelia Rodriguez Escobar <sandranoeliarodriguezescobar@gmail.com>
2022-08-25 14:09:22 -0400

    Se agrego funcion calcularSalario

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ let nombre = "Juan Perez";
 let edad = calcularEdad (2002);
 console.log("Hola " + nombre);
 console.log(nombre + " tiene " + edad + " años");
+console.log("Se le pagara: "+calcularSalario(8, 30000));
 
 function calcularEdad(anioNacimiento){
     return new Date().getFullYear() - anioNacimiento;
+}
+
+function calcularSalario(horasTrabajadas, montoPorHora){
+    return horasTrabajadas * montoPorHora;
 }
